c31193a5be00867dd4a921f275b193630c363e8c
feyijhay <josephfeyishetan@gmail.com>
2024-05-13 12:11:45 +0100

    Updated but not done

--- checkOutApp/CheckOut.go
+++ checkOutApp/CheckOut.go
@@ -8,28 +8,32 @@ import (
 
 var (
 	customerItems []string
-	numberOfItems []int
+	numberOfItems []float64
 	amountOfGoods []float64
 	items         string
-	date          = time.Now()
+	sub           float64
+	subb          float64
+	subTotal      []float64
+
+	date = time.Now()
 )
 
 func main() {
 
-	cashierInput()
+	customerNameHere := ""
 
 	for index := 0; index < math.MaxInt64; index++ {
 		fmt.Println("Customer's name:")
 		var customerName string
 		fmt.Scan(&customerName)
-		fmt.Scanln(&customerName)
+		customerNameHere = customerName
 
 		fmt.Println("Please enter your items:")
 		fmt.Scan(&items)
 		_ = append(customerItems, items)
 
 		fmt.Println("Please enter items quantity:")
-		var itemsNumber int
+		var itemsNumber float64
 		fmt.Scan(&itemsNumber)
 		_ = append(numberOfItems, itemsNumber)
 
@@ -46,30 +50,111 @@ func main() {
 		}
 	}
 
+	value := cashierInput()
+	receipt(value, customerNameHere)
 }
 
-func cashierInput() {
+func cashierInput() string {
 	fmt.Println("Cashier Name:")
 	var cashierName string
 	fmt.Scan(&cashierName)
-	fmt.Scanln(&cashierName)
 
 	fmt.Println("How much discount will the customer get:")
 	var customerDiscount float64
 	fmt.Scan(&customerDiscount)
 
+	return cashierName
 }
 
-func receipt() {
+func receipt(cashierName string, customerName string) {
 	fmt.Println("JHAY'S STORE")
 	fmt.Println("Main Branch")
 	fmt.Println("LOCATION: 312, Herbert Macaulay Way, Sabo Yaba, Lagos")
 	fmt.Println("TEL: 09078480034")
 	date.Format("12-03-1997")
 	fmt.Println("Cashier:", cashierName)
-}
+	fmt.Println("Customer Name:", customerName)
 
-var cashierName string
-fmt.Print("Enter cashier's name: ")
-fmt.Scanln(&cashierName)
-fmt.Println("Cashier:", cashierName)
+	fmt.Println("______________________________________________________________________________________________________________________________________________________")
+	fmt.Println("ITEM         QTY         PRICE         TOTAL(NGN)")
+	fmt.Println("______________________________________________________________________________________________________________________________________________________")
+	for check := 0; check < len(customerItems); check++ {
+		result := customerItems[check]
+		answer := numberOfItems[check]
+		total := amountOfGoods[check]
+		sumOfQuantity := numberOfItems[check] * amountOfGoods[check]
+		fmt.Printf("%s, %f, %f, %f", result, answer, total, sumOfQuantity)
+		subTotal = append(subTotal, sumOfQuantity)
+	}
+	fmt.Println("_______________________________________________________________________________________________________________________________________________________")
+
+	for check := 0; check < len(subTotal); check++ {
+		var sub float64
+		sub = sub + subTotal[check]
+	}
+
+	fmt.Printf("Sub Total: %f ", sub)
+
+	discount := sub * 8 / 100
+	fmt.Printf("Discount: %f", discount)
+
+	VAT := sub * 7.5 / 100
+	fmt.Printf("VAT @ 7.5: %f", VAT)
+
+	fmt.Println("_________________________________________________________________________________________________________________________________________________________")
+	billTotal := (sub + VAT) - discount
+	fmt.Printf("Bill Total: %f", billTotal)
+	fmt.Println("__________________________________________________________________________________________________________________________________________________________")
+
+	fmt.Printf("KINDLY PAY TO THE CASHIER: %f", billTotal)
+
+	fmt.Println("___________________________________________________________________________________________________________________________________________________________")
+
+	fmt.Println("How much did the customer give you?:")
+	var amountPaid float64
+	fmt.Scan(&amountPaid)
+
+	fmt.Println("   ")
+
+	fmt.Println("JHAY'S STORE")
+	fmt.Println("Main Branch")
+	fmt.Println("LOCATION: 312, Herbert Macaulay Way, Sabo Yaba, Lagos")
+	fmt.Println("TEL: 09078480034")
+	date.Format("12-03-1997")
+	fmt.Println("Cashier:", cashierName)
+	fmt.Println("Customer Name:", customerName)
+	fmt.Println("_____________________________________________________________________________________________________________________________________________________________")
+
+	for check := 0; check < len(customerItems); check++ {
+		result := customerItems[check]
+		answer := numberOfItems[check]
+		totall := amountOfGoods[check]
+		sumQuantity := numberOfItems[check] * amountOfGoods[check]
+		fmt.Printf("%s, %f, %f, %f", result, answer, totall, sumQuantity)
+	}
+	fmt.Println("_______________________________________________________________________________________________________________________________________________________________")
+	for check := 0; check < len(subTotal); check++ {
+		var subb float64
+		subb = subb + subTotal[check]
+	}
+
+	fmt.Printf("Sub Total: %f", subb)
+
+	discounted := subb * 8 / 100
+	fmt.Printf("Discount: %f", discounted)
+
+	VAAT := subb * 7.5 / 100
+	fmt.Printf("VAT @ 7.5: %f", VAAT)
+	fmt.Println("____________________________________________________________________________________________________________________________________________________________________")
+	totalAmount := (subb + VAAT) - discounted
+	fmt.Printf("Bill Total: %f", totalAmount)
+
+	fmt.Printf("Amount paid: %f", amountPaid)
+
+	balance := (amountPaid - totalAmount)
+	fmt.Printf("Balance: %f", balance)
+	fmt.Println("_______________________________________________________________________________________________________________________________________________________________________")
+
+	fmt.Println("THANK YOU FOR YOUR PATRONAGE!!!")
+	fmt.Println("________________________________________________________________________________________________________________________________________________________________________")
+}
